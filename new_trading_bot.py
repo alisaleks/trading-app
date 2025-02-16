381ef8784d6a3240f744ab459819a3290f8ee698
alisaleks <162038807+alisaleks@users.noreply.github.com>
2025-02-16 01:23:11 +0100

    Update new_trading_bot.py

--- new_trading_bot.py
+++ new_trading_bot.py
@@ -15,7 +15,7 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # ✅ Proxy Configuration
-PROXY_URL = "http://188.34.156.126:24572"  # Example: Germany Proxy
+PROXY_URL = "http://15.236.203.245:3128"  # Example: Germany Proxy
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
